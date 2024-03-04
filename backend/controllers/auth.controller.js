ddfb61bde53d4843cc642348a35dcbaf9767ecc5
Daniel Dushev <danieldyshew@gmail.com>
2024-03-04 16:23:20 +0200

    auth.controller. add sign up controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,52 @@
-export const signup = (req, res) => {
+import bcrypt from "bcryptjs";
+
+import User from "../models/user.model.js";
+import generateJWTAndSetCookie from "../utils/generateJWT.js";
+
+export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body;
-        console.log(fullName, username, password, confirmPassword, gender);
-        res.send("success");
-    } catch (error) {
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords don't match" });
+        }
 
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exist" });
+        }
+
+        const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? maleProfilePic : femaleProfilePic,
+        });
+
+        if (newUser) {
+            generateJWTAndSetCookie(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                gender: newUser.gender,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
