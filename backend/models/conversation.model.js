022df5e1a08e23b43dcceeba37723cc4ac51cb9d
Daniel Dushev <danieldyshew@gmail.com>
2024-03-10 11:01:33 +0200

    conversation.model. add model

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,10 @@
+import { Schema, model, Types } from "mongoose";
+
+const conversationSchema = new Schema({
+  participants: [{ type: Types.ObjectId, ref: "User" }],
+  messages: [{ type: Types.ObjectId, ref: "Message", default: [] }],
+}, { timestamps: true });
+
+const Conversation = model("Conversation", conversationSchema);
+
+export default Conversation;
