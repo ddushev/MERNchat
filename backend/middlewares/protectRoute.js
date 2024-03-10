e169c5d786769055c136573ac903116e54c5e7fa
Daniel Dushev <danieldyshew@gmail.com>
2024-03-10 12:19:45 +0200

    protectRoute. add middleware to verify jwt

---
+++ backend/middlewares/protectRoute.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return req.status(401).json({ error: "Unauthorized: No Token Provided" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decodedToken) {
+      return req.status(401).json({ error: "Unauthorized: Invalid Token" });
+    }
+
+    const user = await User.findById(decodedToken.userId).select("-password");
+
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+    console.log("Error in protectRoute middleware:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export default protectRoute;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,7 +15,8 @@ dotenv.config();
 //     res.send("Hello MERN chat!");
 // });
 
-app.use(express.json());
+app.use(express.json()); // parse incoming requests with JSON payloads
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
