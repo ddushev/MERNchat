c710c3e55db773f6cecc11d3248d063ab84de3f5
Daniel Dushev <danieldyshew@gmail.com>
2024-03-11 21:20:48 +0200

    message. add get message route and controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,27 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/messege.model.js";
 
+export const getMessage = async (req, res) => {
+  try {
+    const senderId = req.user._id;
+    const { id: userToChatId } = req.params;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+
+  } catch (error) {
+    console.log("Error in getMessage controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessage, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const messageRoutes = Router();
 
+messageRoutes.get("/:id", protectRoute, getMessage);
 messageRoutes.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRoutes;
