c0aa458a85e3452a27f05131f15048845ff76c90
Daniel Dushev <danieldyshew@gmail.com>
2024-03-13 11:58:37 +0200

    messagesList. add component

---
+++ frontend/src/components/messages/messagesList.jsx
@@ -0,0 +1,21 @@
+import Message from "./message";
+
+const MessagesList = () => {
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+export default MessagesList;
