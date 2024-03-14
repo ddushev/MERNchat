a88aae329bb0ccec4db7f2d11ef0f54fa12d0ade
Daniel Dushev <danieldyshew@gmail.com>
2024-03-14 14:48:02 +0200

    useLogout. add logout hook and functionality

--- frontend/src/components/sidebar/logoutButton.jsx
+++ frontend/src/components/sidebar/logoutButton.jsx
@@ -1,9 +1,18 @@
 import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+  const { isLoading, logout } = useLogout();
   return (
     <div className="mt-auto">
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+      {isLoading ? (
+        <span className="loading loading-spinner"></span>
+      ) : (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      )}
     </div>
   );
 };
