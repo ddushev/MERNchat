bdf5b104599e3dc478e8b40c5db1ba8733234143
Daniel Dushev <danieldyshew@gmail.com>
2024-03-15 18:33:24 +0200

    socket. add socket init setup on backend

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,20 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json()); // parse incoming requests with JSON payloads
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
