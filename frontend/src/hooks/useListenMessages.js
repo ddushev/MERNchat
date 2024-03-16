53dbbaa3fa40baa9ab2f83c09092a9a4c006eda0
Daniel Dushev <danieldyshew@gmail.com>
2024-03-16 17:54:14 +0200

    useListenMessage. only listen for messages from senders which are current selected conversation

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -6,17 +6,20 @@ import notificationSound from "../assets/sounds/notification.mp3"
 const useListenMessages = () => {
   const { socket } = useSocketContext();
   const { messages, setMessages } = useConversation();
+  const { selectedConversation } = useConversation();
 
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
-      newMessage.shouldShake = true;
-      const sound = new Audio(notificationSound);
-      sound.play();
-      setMessages([...messages, newMessage]);
+      if (selectedConversation._id === newMessage.senderId) {
+        newMessage.shouldShake = true;
+        const sound = new Audio(notificationSound);
+        sound.play();
+        setMessages([...messages, newMessage]);
+      }
     })
 
     return () => socket?.off("newMessage");
-  }, [socket, setMessages, messages]);
+  }, [socket, setMessages, messages, selectedConversation]);
 }
 
 export default useListenMessages;
